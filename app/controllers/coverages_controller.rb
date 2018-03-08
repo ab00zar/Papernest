df6d1e9c6c9157d07aaf28ba9df3c8e3114fa5d7
Aboozar Rajabi <rajabi.aboozar@gmail.com>
2018-03-08 19:13:20 +0100

    closest method & its route are added

--- app/controllers/coverages_controller.rb
+++ app/controllers/coverages_controller.rb
@@ -1,2 +1,14 @@
+require 'json'
+
 class CoveragesController < ApplicationController
+    
+    # GET /closest/:id It returns the results using the closest registered locations in the db to the searching address
+    def closest
+        @coverage = Coverage.near(params[:id], 10, :units => :km, :order => "distance")
+        result = Hash.new
+        @coverage.each do |c|
+            result.reverse_merge!({c.operator => { "2G" => c.g2, "3G" => c.g3, "4G" => c.g4, "distance" => c.distance}})
+        end
+        render json: result
+    end
 end
